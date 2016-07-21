bf1edfbef008c9bb67c029ae0c3f90fd011fc38a
Temitope Olotin <temitopeolotin@Temitopes-MacBook-Pro-2.local>
2016-07-21 10:57:00 +0100

    Add todo service

--- src/app/todo-app/todo-app.component.ts
+++ src/app/todo-app/todo-app.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {TodoService} from '../todo.service';
 
 @Component({
   moduleId: module.id,
   selector: 'app-todo-app',
   templateUrl: 'todo-app.component.html',
-  styleUrls: ['todo-app.component.css']
+  styleUrls: ['todo-app.component.css'],
+  providers: [TodoService]
 })
 export class TodoAppComponent implements OnInit {
 
